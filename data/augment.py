194d9f156ebf674148e00043d68ee417b977dd22
Augustin Laurent <augustin.lrt2@icloud.com>
2024-05-28 11:12:36 +0200

    Fix loading issues where id where not splitted correctly, remove PieceWiseAffine temporaly to replace it with ElasticTransform to speed up the augmentations

--- data/augment.py
+++ data/augment.py
@@ -20,7 +20,7 @@ def augment_data(image: np.ndarray, mask: np.ndarray) -> tuple:
         A.VerticalFlip(p=0.5),
         A.OneOf([A.ElasticTransform(),
                  A.Perspective(scale=(0.05, 0.1)),
-                 A.PiecewiseAffine(scale=(0.03, 0.05)),
+                 #A.PiecewiseAffine(scale=(0.03, 0.05)),
                  A.ShiftScaleRotate(shift_limit=0.15, scale_limit=0.4, rotate_limit=45)],
                 p=0.5),
         A.OneOf([A.GridDistortion(),
